24ef5d17620eae503eadc2f372844fa99d37a1e4
hpruthi1 <hardikpruthi177@gmail.com>
2024-02-02 02:00:44 +0530

    Update DistalResection.tsx

--- src/components/right-container/DistalResection.tsx
+++ src/components/right-container/DistalResection.tsx
@@ -6,14 +6,26 @@ const DistalResection = () => {
     <>
       <h3>Distal Resection</h3>
       <ButtonGroup size="small" aria-label="small outlined button group">
-        <Button>+</Button>
+        <Button
+          onClick={() => {
+            setresection(resection + 1);
+          }}
+        >
+          +
+        </Button>
         <input
           style={{ textAlign: "center" }}
           type="text"
           value={resection + "mm"}
           disabled
         ></input>
-        <Button>-</Button>
+        <Button
+          onClick={() => {
+            setresection(resection - 1);
+          }}
+        >
+          -
+        </Button>
       </ButtonGroup>
     </>
   );
